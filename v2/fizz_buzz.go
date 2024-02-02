6f212d5cbef43ca02fc0d446ae55690166254d0c
Alejandro Falkowski <alexrfalkowski@gmail.com>
2024-02-02 06:09:06 +0100

    refactor: add clearer var names

--- v2/fizz_buzz.go
+++ v2/fizz_buzz.go
@@ -2,17 +2,17 @@ package v2
 
 // NewFizzBuzz game.
 func NewFizzBuzz() *Game {
-	return NewGame([]Rule{fizzBuzz, fizz, buzz, none})
+	return NewGame([]Rule{fizzBuzz, fizz, buzz, number})
 }
 
-func fizz(n int) (string, bool) {
-	return "Fizz", n%3 == 0
+func fizz(number int) (string, bool) {
+	return "Fizz", number%3 == 0
 }
 
-func buzz(n int) (string, bool) {
-	return "Buzz", n%5 == 0
+func buzz(number int) (string, bool) {
+	return "Buzz", number%5 == 0
 }
 
-func fizzBuzz(n int) (string, bool) {
-	return "Fizz Buzz", n%15 == 0
+func fizzBuzz(number int) (string, bool) {
+	return "Fizz Buzz", number%15 == 0
 }
